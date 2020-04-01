f7d7af93cdb4f73c4a26158d175c92bd40754dba
leonardo pernett jimenez <pernettleonardo@gmail.com>
2020-03-31 20:28:25 -0500

    typescript y react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useRef } from "react";
 
-function App() {
+import "./App.css";
+
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+type formElement = React.FormEvent<HTMLFormElement>;
+
+function App(): JSX.Element {
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  const inputFocus = useRef<HTMLInputElement>(null)
+
+  const handlerSubmit = (e: formElement) => {
+    e.preventDefault();
+    addTask(task);
+    setTask("");
+    inputFocus.current?.focus();
+  }
+
+  const addTask = (task: string) => {
+    const newTask: ITask[] = [...tasks, { name: task, done: false }];
+    setTasks(newTask);
+  }
+
+
+  const toggleTask = (i:number) => {
+     const newTask = [...tasks];
+     newTask[i].done = !newTask[i].done
+     setTasks(newTask)
+  };
+
+  const removeTask = (i:number): void =>{
+     const newTask =  [...tasks]
+     newTask.splice(i,1)
+     setTasks(newTask)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <div className="card mt-5">
+              <div className="card-body">
+                <form onSubmit={handlerSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      ref={inputFocus}
+                      value={task}
+                      onChange={e => setTask(e.target.value)}
+                    />
+                  </div>
+                  <button className="btn btn-primary btn-block">save</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            {tasks.map((task, i) => (
+              <div className="card my-1" key={i}>
+                <div className="card-body text-center">
+                  <h2
+                    style={{ textDecoration: task.done ? "line-through" : "" }}
+                  >
+                    {task.name}
+                  </h2>
+                  <p>{task.done + ""}</p>
+                  <button 
+                  onClick={()=>toggleTask(i)}
+                  className="btn btn-secondary">
+                    {task.done ? "✓" : "✗"}
+                  </button>
+                  <button onClick={()=>removeTask(i)}>
+                     <i className="fa fa-trash-o"></i>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
